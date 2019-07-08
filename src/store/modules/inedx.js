f29e759c0e0a957fa1d7ba5b41c3255a1c10e6d6
dingbaba <2480790748@qq.com>
2019-07-08 20:09:03 +0800

    首页

--- src/store/modules/inedx.js
+++ src/store/modules/inedx.js
@@ -1,20 +1,59 @@
-import { topAll } from '../../server/index'
+import { topAll, contentAll } from '../../server/index'
 const state = {
-
+  topAll: [],
+  contnetAll: []
 }
 
 const actions = {
+  //头部数据
   async scrollAll ({ commit }, options) {
-    console.log(topAll)
-    let data = await topAll()
-    console.log(data)
-
-    return data
-  }
+    // var data = await topAll(options)
+    // console.log(data, '...............')
+    wx.request({
+      url: 'https://upapi.jinaup.com/api/open/product/category/sub/1', //开发者服务器接口地址",
+      data: options, //请求的参数",
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+      success: res => {
+        if (res.data.res_code) {
+          commit('topAll', res.data.result)
+        }
+      }
+    });
+  },
+  // 轮播图片
+  async bannerimgUrl ({ commit }, options) {
+    // var data = await contentAll(options)
+    // console.log(data, '...............')
+    wx.request({
+      url: 'https://upapi.jinaup.com/api/open/page/home/list/1.0.0', //开发者服务器接口地址",
+      data: options, //请求的参数",
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+      success: res => {
+        if (res.data.res_code) {
+          commit('contentAll', res.data.result)
+        }
+      }
+    });
+  },
 }
 
 const mutations = {
-
+  topAll (state, options) {
+    console.log(options)
+    state.topAll = options
+  },
+  contentAll (state, options) {
+    console.log(options)
+    state.contnetAll = options
+  },
 }
 
 export default {
